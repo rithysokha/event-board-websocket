b256fb25106eb18c1646f039323c10083f2aa6ee
Sokha Rithy <124172865+rithysokha@users.noreply.github.com>
2024-12-09 21:44:45 +0700

    can generate qr code

---
+++ pages/qrcode.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="qrcode"></div>
+</template>
+
+<script>
+import QRCodeStyling from 'qr-code-styling';
+
+export default {
+  name: "QrCode",
+  mounted() {
+    const qrCode = new QRCodeStyling({
+      width: 300,
+      height: 300,
+      data: "https://youtu.be/qReulBcm3Og?si=XIMICHNHU3wIsbG9",
+      // image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.m.wikipedia.org%2Fwiki%2FFile%3ANpm-logo.svg&psig=AOvVaw3U9kn4Z7kjWJjsWOaX2v0-&ust=1733841491423000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPimqIX1mooDFQAAAAAdAAAAABAE",
+      dotsOptions: {
+        color: "#4267b2",
+        type: "rounded"
+      },
+      backgroundOptions: {
+        color: "#e9ebee",
+      },
+      imageOptions: {
+        crossOrigin: "anonymous",
+        margin: 20
+      }
+    });
+
+    qrCode.append(document.getElementById("qrcode"));
+  }
+};
+</script>
+
+<style scoped>
+#qrcode {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+}
+</style>
