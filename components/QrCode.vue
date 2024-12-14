36b40eaeee41dfd691ff2ec0b70c331ba835ec8d
Sokha Rithy <124172865+rithysokha@users.noreply.github.com>
2024-12-15 01:54:38 +0700

    edit qr code

--- components/QrCode.vue
+++ components/QrCode.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-
+const config = useRuntimeConfig()
 const props = defineProps<{
   boardId: string
   width: number
@@ -13,7 +13,7 @@ onMounted(() => {
   const qrCode = new QRCodeStyling({
     width: props.width,
     height: props.height,
-    data: `http://localhost/board/${props.boardId}`,
+    data: `${config.public.baseUrl}/board/${props.boardId}`,
     // image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.  m.wikipedia.org%2Fwiki%2FFile%3ANpm-logo.svg&psig=AOvVaw3U9kn4Z7kjWJjsWOaX2v0-&ust=1733841491423000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPimqIX1mooDFQAAAAAdAAAAABAE",
     dotsOptions: {
       color: "#4267b2",
