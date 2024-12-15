51a6c6e806817d893788a243d907127c3f250c8a
Sokha Rithy <124172865+rithysokha@users.noreply.github.com>
2024-12-15 11:40:30 +0700

    image compression

--- components/board/Post.vue
+++ components/board/Post.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import imageCompression from 'browser-image-compression';
 const props = defineProps<{
   boardId: string
 }>();
@@ -18,19 +19,49 @@ const handleUploadFile = async (event: any) => {
     console.error('No file selected');
     return;
   }
-  isUploadingPhoto.value = true
-  const file = event.target.files[0];
+
+  isUploadingPhoto.value = true;
+  const file = files[0];
+
+  if (file.size > 1048576) {
+    console.log('File is larger than 1MB, compressing...');
+
+    // Compression options
+    const options = {
+      maxSizeMB: 1,
+      useWebWorker: true,
+    };
+
+    try {
+      const compressedFile = await imageCompression(file, options);
+      console.log('Compression successful:', compressedFile);
+
+      await uploadFile(compressedFile);
+    } catch (error) {
+      console.error('Compression Error:', error);
+      isUploadingPhoto.value = false;
+      return;
+    }
+  } else {
+    await uploadFile(file);
+  }
+};
+
+const uploadFile = async (file: File) => {
   const formData = new FormData();
   formData.append('file', file);
+
   try {
     const res: { public_id: string } = await $fetch('/api/image', {
       method: 'POST',
       body: formData,
     });
-    postBody.value.imgPublicId = res.public_id
-    isUploadingPhoto.value = false
+    postBody.value.imgPublicId = res.public_id;
+    console.log('File uploaded successfully:', res.public_id);
   } catch (error) {
     console.error('Upload Error:', error);
+  } finally {
+    isUploadingPhoto.value = false;
   }
 };
 
