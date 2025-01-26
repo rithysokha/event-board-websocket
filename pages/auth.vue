f7aa4d9e569bf8dba456a85ef44dff6478c993c8
Rithy <thycambodi@gmail.com>
2025-01-26 15:51:13 +0700

    auth screen

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,8 +1,28 @@
 <script setup lang="ts">
 definePageMeta({
-  auth: { 
-    unauthenticatedOnly: true, 
-    navigateAuthenticatedTo: '/dashboard/home' }
+  auth: {
+    unauthenticatedOnly: true,
+    navigateAuthenticatedTo: '/dashboard/home'
+  }
+})
+import lottie from 'lottie-web'
+import art from "../assets/user_research.json"
+const isLoading = ref(true)
+
+const animationContainer = ref<HTMLElement | null>(null)
+onMounted(() => {
+  if (animationContainer.value) {
+    const animation = lottie.loadAnimation({
+      container: animationContainer.value,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: art
+    })
+    animation.addEventListener('DOMLoaded', () => {
+      isLoading.value = false
+    })
+  }
 })
 const { signIn } = useAuth()
 import googleIcon from "../assets/google.svg";
@@ -16,7 +36,7 @@ const items = [{
   description: 'Change your signup here. After saving, you\'ll be logged out.'
 }]
 
-const loginForm = reactive({ username: '', password: ''})
+const loginForm = reactive({ username: '', password: '' })
 const signUpForm = reactive({ username: '', password: '', confirmPassword: '' })
 const errorMessage = ref('')
 const loading = ref(false)
@@ -46,61 +66,66 @@ const onSubmit = async (form: any) => {
 </script>
 
 <template>
-  <div class="p-2">
-  <UTabs :items="items" class="w-full">
-    <template #item="{ item }">
-      <UCard @submit.prevent="() => onSubmit(item.key === 'login' ? loginForm
-       : signUpForm
-      )">
-        <template #header>
-          <p class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
-            {{ item.label }}
-          </p>
-          <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">
-            {{ item.description }}
-          </p>
-        </template>
+  <div class="p-2 md:grid md:grid-cols-2 md:h-screen">
+    <div class="hidden md:flex md:justify-center md:items-center" >
+    <USkeleton class="h-2/5 w-4/6 lg:h-3/5 lg:w-5/6 rounded-md" v-show="isLoading"/>
+    <div v-show="!isLoading" class=" h-3/4 w-4/5 lg:h-full lg:w-full " ref="animationContainer"></div>
+    </div>
+    <div class="flex md:justify-end md:items-center sm:mr-12 md:mr-24 ">
+    <UTabs :items="items" class="w-full md:max-w-96">
+      <template #item="{ item }">
+        <UCard @submit.prevent="() => onSubmit(item.key === 'login' ? loginForm
+          : signUpForm
+        )">
+          <template #header>
+            <p class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
+              {{ item.label }}
+            </p>
+            <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">
+              {{ item.description }}
+            </p>
+          </template>
 
-        <div v-if="item.key === 'login'" class="space-y-3">
-          <UFormGroup label="Username" name="username">
-            <UInput v-model="loginForm
-          .username" />
-          </UFormGroup>
-          <UFormGroup label="Password" name="password">
-            <UInput v-model="loginForm
-          .password" type="password"/>
-          </UFormGroup>
-        </div>
-        <div v-else-if="item.key === 'signup'" class="space-y-3">
-          <UFormGroup label="Username" name="username" required>
-            <UInput v-model="signUpForm
-          .username" type="text" required />
-          </UFormGroup>
-          <UFormGroup label="Password" name="password" required>
-            <UInput v-model="signUpForm
-          .password" type="password" required />
-          </UFormGroup>
-          <UFormGroup label="Confirm Password" name="conPassword" required>
-            <UInput v-model="signUpForm
-          .confirmPassword" type="password" required />
-          </UFormGroup>
-        </div>
-        <template #footer>
-          <div class="flex justify-center">
-            <UButton type="submit">
-              {{ item.key === 'login' ? 'login' : 'signup' }}
-            </UButton>
+          <div v-if="item.key === 'login'" class="space-y-3">
+            <UFormGroup label="Username" name="username">
+              <UInput v-model="loginForm
+                .username" />
+            </UFormGroup>
+            <UFormGroup label="Password" name="password">
+              <UInput v-model="loginForm
+                .password" type="password" />
+            </UFormGroup>
           </div>
-        </template>
-      </UCard>
-      <div class="pt-10">
-        <UButton block @click="signIn('google')">
-          <img :src="googleIcon" class="w-5" alt="google logo, webboard"/>
-          <p>Continue with google</p>
-        </UButton>
-      </div>
-    </template>
-  </UTabs>
-</div>
+          <div v-else-if="item.key === 'signup'" class="space-y-3">
+            <UFormGroup label="Username" name="username" required>
+              <UInput v-model="signUpForm
+                .username" type="text" required />
+            </UFormGroup>
+            <UFormGroup label="Password" name="password" required>
+              <UInput v-model="signUpForm
+                .password" type="password" required />
+            </UFormGroup>
+            <UFormGroup label="Confirm Password" name="conPassword" required>
+              <UInput v-model="signUpForm
+                .confirmPassword" type="password" required />
+            </UFormGroup>
+          </div>
+          <template #footer>
+            <div class="flex justify-center">
+              <UButton type="submit">
+                {{ item.key === 'login' ? 'login' : 'signup' }}
+              </UButton>
+            </div>
+          </template>
+        </UCard>
+        <div class="pt-10">
+          <UButton block @click="signIn('google')">
+            <img :src="googleIcon" class="w-5" alt="google logo, webboard" />
+            <p>Continue with google</p>
+          </UButton>
+        </div>
+      </template>
+    </UTabs>
+  </div>
+  </div>
 </template>
-
