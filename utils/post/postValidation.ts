ea034f7ec8ad4fd61c10ee5d4a7ed9c2678cad64
Rithy <thycambodi@gmail.com>
2025-04-06 21:02:03 +0700

    extend length of title and description

--- utils/post/postValidation.ts
+++ utils/post/postValidation.ts
@@ -2,11 +2,11 @@ import Joi from "joi";
 
 export const postSchema = Joi.object({
   boardId: Joi.string().required(),
-  title: Joi.string().max(30).allow(''),
+  title: Joi.string().max(500).allow(''),
   imgPublicId: Joi.string().allow(''),
   imgHeigh: Joi.number(),
   imgWidth: Joi.number(),
-  description: Joi.string().max(100).allow(''),
+  description: Joi.string().max(1000).allow(''),
   likes: Joi.number().default(0),
   commentCount: Joi.number().default(0),
   postedBy: Joi.string().max(30),
