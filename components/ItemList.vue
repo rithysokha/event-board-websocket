5db580f269fa4eb4fbaa59571534f97dc264d0a6
Rithy <thycambodi@gmail.com>
2025-05-15 20:53:03 +0700

    improve item list ui

--- components/ItemList.vue
+++ components/ItemList.vue
@@ -16,7 +16,7 @@ const isProcessing = ref(false)
 const getDeleteUrl = (place: string) => {
   if (place == 'mine')
     return 'board'
-  else if ( place=='recent')
+  else if (place == 'recent')
     return 'recent'
   else return 'board/permanent'
 }
@@ -30,8 +30,8 @@ const handleDeleteBoard = async () => {
     // @ts-expect-error
     if (res?.statusCode == 200) {
       const index = props.items.findIndex(item => item._id === boardIdToDelete.value)
-      if(props.place==='mine'){
-      itemStore.addTrash(props.items[index].background,props.items[index].name,props.items[index]._id,props.items[index].boardId )
+      if (props.place === 'mine') {
+        itemStore.addTrash(props.items[index].background, props.items[index].name, props.items[index]._id, props.items[index].boardId)
       }
       if (index !== -1) {
         props.items.splice(index, 1)
@@ -53,7 +53,7 @@ const hanldeRestoreBoard = async () => {
     })
     if (res?.statusCode == 200) {
       const index = props.items.findIndex(item => item._id === boardIdToRestore.value)
-      itemStore.addMine(props.items[index].background,props.items[index].name,props.items[index]._id,props.items[index].boardId )
+      itemStore.addMine(props.items[index].background, props.items[index].name, props.items[index]._id, props.items[index].boardId)
       if (index !== -1) {
         props.items.splice(index, 1)
       }
@@ -77,39 +77,43 @@ const handleDisplayRestoringPrompt = (boardId: string) => {
 </script>
 <template>
   <ClientOnly>
-  <UModal v-model="isOpenDeleteBoard">
-    <div class="p-4 m-4 text-center">
-      <p class="text-red-500 font-bold mb-6">
-        Are you sure to delete this item?
-      </p>
-      <div class="flex justify-center gap-4">
-        <UButton class="w-1/4 flex justify-center" label="No" @click="isOpenDeleteBoard = false" />
-        <UButton :loading="isProcessing" class="w-1/4 flex justify-center" color="red" icon="i-heroicons-trash"
-          label="Yes sure!" @click="handleDeleteBoard" />
+    <UModal v-model="isOpenDeleteBoard">
+      <div class="p-4 m-4 text-center">
+        <p class="text-red-500 font-bold mb-6">
+          Are you sure to delete this item?
+        </p>
+        <div class="flex justify-center gap-4">
+          <UButton class="w-1/4 flex justify-center" label="No" @click="isOpenDeleteBoard = false" />
+          <UButton :loading="isProcessing" class="w-1/4 flex justify-center" color="red" icon="i-heroicons-trash"
+            label="Yes sure!" @click="handleDeleteBoard" />
+        </div>
       </div>
-    </div>
-  </UModal>
-  <UModal v-model="isOpenRestoreBoard">
-    <div class="p-4 m-4 text-center">
-      <p class="text-red-500 font-bold mb-6">
-        Are you sure to restore this item?
-      </p>
-      <div class="flex justify-center gap-4">
-        <UButton class="w-1/4 flex justify-center" label="No" @click="isOpenRestoreBoard = false" />
-        <UButton :loading="isProcessing" class="w-1/4 flex justify-center" color="red" icon="i-heroicons-trash"
-          label="Yes sure!" @click="hanldeRestoreBoard" />
+    </UModal>
+    <UModal v-model="isOpenRestoreBoard">
+      <div class="p-4 m-4 text-center">
+        <p class="text-red-500 font-bold mb-6">
+          Are you sure to restore this item?
+        </p>
+        <div class="flex justify-center gap-4">
+          <UButton class="w-1/4 flex justify-center" label="No" @click="isOpenRestoreBoard = false" />
+          <UButton :loading="isProcessing" class="w-1/4 flex justify-center" color="red" icon="i-heroicons-trash"
+            label="Yes sure!" @click="hanldeRestoreBoard" />
+        </div>
       </div>
-    </div>
-  </UModal>
-  <UContainer>
-    <div class="grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      <UCard v-for="item in items" :key="item.name" class="w-full">
-        <div class="flex justify-between  sm:w-full sm:h-28 md:h-40 sm:flex-col sm:items-center ">
-          <div class="w-10 rounded-md sm:w-full sm:h-full cursor-pointer"
-            @click="navigateBoard(place == 'mine' ? item._id : item.boardId)" :class="'bg-' + item.background"></div>
-          <div class="flex w-full justify-between">
-            <p class="w-full text-end sm:text-start">{{ item.name }}</p>
-            <UDropdown :items="[
+    </UModal>
+    <UContainer>
+      <div class="grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <UCard v-for="item in items" :key="item.name" class="w-full">
+          <div class="flex justify-between w-full sm:items-end sm:pl-2">
+          <div class="flex justify-between items-center w-5/6 sm:w-full sm:h-28 md:h-40 sm:flex-col"
+            @click="navigateBoard(place == 'mine' ? item._id : item.boardId)">
+            <div class="w-4/5 h-12 rounded-md sm:w-full sm:h-full cursor-pointer" :class="'bg-' + item.background">
+            </div>
+            <div class="flex w-full justify-between">
+              <p class="w-full text-end sm:text-start">{{ item.name }}</p>
+            </div>
+          </div>
+          <UDropdown class="sm:w-2" :items="[
               [
                 {
                   label: 'Delete',
@@ -127,9 +131,8 @@ const handleDisplayRestoringPrompt = (boardId: string) => {
               <UButton color="white" trailing-icon="i-heroicons-ellipsis-vertical" variant="ghost" />
             </UDropdown>
           </div>
-        </div>
-      </UCard>
-    </div>
-  </UContainer>
-</ClientOnly>
+        </UCard>
+      </div>
+    </UContainer>
+  </ClientOnly>
 </template>
